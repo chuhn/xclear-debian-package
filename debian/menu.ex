1064ea6e468f652de98ca3b548c19f5daa5bdfc0
Christopher Huhn <C.Huhn@gsi.de>
2014-02-08 13:53:15 +0100

    Cleanup unused dh_make templates

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(xul-ext-xclear):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="xul-ext-xclear" command="/usr/bin/xul-ext-xclear"
--- debian/xul-ext-xclear.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the xul-ext-xclear package
-#
-0 4	* * *	root	[ -x /usr/bin/xul-ext-xclear_maintenance ] && /usr/bin/xul-ext-xclear_maintenance
